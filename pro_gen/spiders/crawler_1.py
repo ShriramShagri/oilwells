ca68934b3a76d1f51c34fac1de51483769b3ec34
Sumith S <sumith2620@gmail.com>
2020-09-30 23:50:23 +0530

    more details added

--- pro_gen/spiders/crawler_1.py
+++ pro_gen/spiders/crawler_1.py
@@ -3,6 +3,7 @@ from scrapy.http import FormRequest
 # from ..items import TutorialItem
 from ..constants import *
 
+
 class Crawler(scrapy.Spider):
     name = CRAWLER_NAME
     start_urls = [
@@ -11,33 +12,40 @@ class Crawler(scrapy.Spider):
 
     def parse(self, response):
         return FormRequest.from_response(response, formdata={
-            'ew' : 'W',
+            'ew': 'W',
             'f_st': '15',
-            'f_c': '3'
-        }, callback = self.start_scraping)
+            'f_c': '39'
+        }, callback=self.start_scraping)
 
-    
     def start_scraping(self, response):
-        # items = TutorialItem()
+        all_div = response.css("tr~ tr+ tr a:nth-child(1)").xpath("@href").extract()
 
-        all_div = response.css('tr~ tr+ tr a:nth-child(1)').xpath("@href").extract()
-        
-        yield response.follow('https://chasm.kgs.ku.edu/ords/qualified.well_page.DisplayWell?f_kid=1041231775', callback=self.get_page_data)
+        # for a in all_div:
+        yield response.follow(all_div[5], callback=self.get_data)
 
-    
-    def get_page_data(self, response):
-        pagedata = dict()
+    def get_data(self, response):
+        page_data = dict()
         # Add main table
-        table_elem = response.css('table:nth-child(5) tr:nth-child(1) td::text').extract()
-        if table_elem:
-            pagedata['maintable'] = table_elem
+        well_data = response.css('hr+ table tr:nth-child(1) td::text').extract()
+        well_data2 = response.css("table:nth-child(5) tr:nth-child(1) td::text").extract()
 
-        h3tags = response.css('h3::text').extract()
-        atags = response.css('h3+ p a').xpath("@href").extract()
-        # if 'Oil Production Data' in h3tags:
-        #     pagedata['oil_production_data'] = h3tags
-            # pagedata['oil_production_data'] = oil_production_data
-        if atags:
-            pagedata['oil_production_data'] = atags
+        if well_data:
+            page_data['well_data'] = well_data
+        if well_data2:
+            page_data['well_data'] = well_data2
 
-        yield {'elems' : pagedata}
+        cutting = response.css('table:nth-child(8) td::text').extract()
+        if cutting:
+            page_data['cutting'] = cutting
+
+        intent = response.css("tr+ tr li a").xpath("@href").extract()
+        intent_name = response.css("tr+ tr li a::text").extract()
+
+        if intent:
+            page_data['intent'] = intent
+        oil_production = response.css('h3+ p a').xpath("@href").extract()
+
+        if oil_production:
+            page_data['oil_production_data'] = oil_production
+
+        yield {'elems': page_data}
