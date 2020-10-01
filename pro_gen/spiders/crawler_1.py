38a3e1b839ddb86056f75604ff1edc7c4a7d6d77
Sumith S <sumith2620@gmail.com>
2020-10-01 13:50:33 +0530

    engineering data added

--- pro_gen/spiders/crawler_1.py
+++ pro_gen/spiders/crawler_1.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy.http import FormRequest
-# from ..items import TutorialItem
 from ..constants import *
 
 
@@ -14,7 +13,8 @@ class Crawler(scrapy.Spider):
         return FormRequest.from_response(response, formdata={
             'ew': 'W',
             'f_st': '15',
-            'f_c': '39'
+            'f_c': '205',
+            'f_pg': '3'
         }, callback=self.start_scraping)
 
     def start_scraping(self, response):
@@ -24,15 +24,80 @@ class Crawler(scrapy.Spider):
         yield response.follow(all_div[5], callback=self.get_data)
 
     def get_data(self, response):
+        tabl = response.css("hr+ table a").xpath("@href").extract()
+        tabls = response.css("hr+ table a::text").extract()
+
+        print(tabls)
+
+        if tabls[0] == "View Engineering Data" and tabl:
+            yield response.follow(tabl[0], callback=self.get_tables)
+        else:
+
+            page_data = dict()
+            # Add main table
+            well_data = response.css('hr+ table tr:nth-child(1) td::text').extract()
+            well_data2 = response.css("table:nth-child(5) tr:nth-child(1) td::text").extract()
+
+            if well_data:
+                page_data['well_data'] = well_data
+            if well_data2:
+                page_data['well_data'] = well_data2
+
+            initial_potential = response.css('table:nth-child(7) td+ td::text').extract()
+
+            if initial_potential:
+                page_data['IP']: initial_potential
+
+            casing_data = response.css("table:nth-child(9) th , table:nth-child(9) tr+ tr td::text").extract()
+
+            if casing_data:
+                page_data['casing'] = casing_data
+
+            perforation_data = response.css("table:nth-child(13) tr+ tr td , table:nth-child(13) th::text").extract()
+
+            if perforation_data:
+                page_data['PF'] = perforation_data
+
+            cutting = response.css('table:nth-child(8) td::text').extract()
+            if cutting:
+                page_data['cutting'] = cutting
+
+            intent = response.css("tr+ tr li a").xpath("@href").extract()
+            intent_name = response.css("tr+ tr li a::text").extract()
+
+            if intent:
+                page_data['intent'] = intent
+
+            oil_production = response.css('h3+ p a').xpath("@href").extract()
+            if oil_production:
+                page_data['oil_production_data'] = oil_production
+
+            yield {'elems': page_data}
+
+    def get_tables(self, response):
         page_data = dict()
         # Add main table
         well_data = response.css('hr+ table tr:nth-child(1) td::text').extract()
         well_data2 = response.css("table:nth-child(5) tr:nth-child(1) td::text").extract()
-
         if well_data:
             page_data['well_data'] = well_data
         if well_data2:
             page_data['well_data'] = well_data2
+
+        initial_potential = response.css('table:nth-child(7) td+ td::text').extract()
+
+        if initial_potential:
+            page_data['IP']: initial_potential
+
+        casing_data = response.css("table:nth-child(9) th::text , table:nth-child(9) tr+ tr td::text").extract()
+
+        if casing_data:
+            page_data['casing'] = casing_data
+
+        perforation_data = response.css("table:nth-child(13) td::text , table:nth-child(13) th::text").extract()
+
+        if perforation_data:
+            page_data['PF'] = perforation_data
 
         cutting = response.css('table:nth-child(8) td::text').extract()
         if cutting:
@@ -43,8 +108,8 @@ class Crawler(scrapy.Spider):
 
         if intent:
             page_data['intent'] = intent
-        oil_production = response.css('h3+ p a').xpath("@href").extract()
 
+        oil_production = response.css('h3+ p a').xpath("@href").extract()
         if oil_production:
             page_data['oil_production_data'] = oil_production
 
