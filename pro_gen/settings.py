2f07da191b820a5af2531403b8f6c56d6b603d0a
ShriramShagri <shagrisrm@gmail.com>
2020-10-01 20:38:25 +0530

    pipline enabled

--- pro_gen/settings.py
+++ pro_gen/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'pro_gen.pipelines.ProGenPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'pro_gen.pipelines.ProGenPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
