63b5966e741a424792e2113d4d252fff67f837d8
ShriramShagri <shagrisrm@gmail.com>
2020-10-01 20:38:05 +0530

    items added

--- pro_gen/items.py
+++ pro_gen/items.py
@@ -8,5 +8,8 @@ import scrapy
 
 class ProGenItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    wh = scrapy.Field()
+    ip = scrapy.Field()
+    casing = scrapy.Field()
+    pf = scrapy.Field()
+    cutting = scrapy.Field()
