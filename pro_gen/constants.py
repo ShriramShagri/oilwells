27496a05bf4a3f0043e57dc5b3fe3efc9d128978
ShriramShagri <shagrisrm@gmail.com>
2020-10-02 19:04:14 +0530

    Added todownload

--- pro_gen/constants.py
+++ pro_gen/constants.py
@@ -9,6 +9,8 @@ DATABASE = 'oilwells'
 COUNTY = (3, 7, 11)
 
 
+TODOWNLOAD = ['Intent To Drill Well', 'Well Completion Report', 'Pit Closure - Drilling Pit']
+
 # crawler name
 
 CRAWLER_NAME = 'genx'
