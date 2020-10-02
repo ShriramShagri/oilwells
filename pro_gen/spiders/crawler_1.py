33113d9cba34e82b70644957620120d027780e2f
Sumith S <sumith2620@gmail.com>
2020-10-02 14:23:40 +0530

    abc

--- pro_gen/spiders/crawler_1.py
+++ pro_gen/spiders/crawler_1.py
@@ -3,7 +3,6 @@ from scrapy.http import FormRequest
 from ..items import ProGenItem
 from ..constants import *
 import os
-from scrapy.http import Request
 
 
 class Crawler(scrapy.Spider):
@@ -66,14 +65,9 @@ class Crawler(scrapy.Spider):
             if cutting:
                 self.items['cutting'] = cutting
 
-            intent = response.css("li:nth-child(1) a").xpath("@href").extract()
+            intent = response.css("tr+ tr li a").xpath("@href").extract()
             intent_name = response.css("tr+ tr li a::text").extract()
-            print(intent)
-            for href in response.css("li:nth-child(1) a").xpath("@href").extract():
-                yield Request(
-                    url=response.urljoin(href),
-                    callback=self.save_pdf
-                )
+
             if intent:
                 page_data['intent'] = intent
 
@@ -112,14 +106,9 @@ class Crawler(scrapy.Spider):
         if cutting:
             self.items['cutting'] = cutting
 
-        intent = response.css("li:nth-child(1) a").xpath("@href").extract()
+        intent = response.css("tr+ tr li a").xpath("@href").extract()
         intent_name = response.css("tr+ tr li a::text").extract()
-        print(intent)
-        for href in response.css("li:nth-child(1) a").xpath("@href").extract():
-            yield Request(
-                url=response.urljoin(href),
-                callback=self.save_pdf
-            )
+
         if intent:
             page_data['intent'] = intent
 
@@ -129,9 +118,13 @@ class Crawler(scrapy.Spider):
 
         yield self.items
 
-    def save_pdf(self, response):
-        path = os.path.join(os.getcwd(), "docs", "123.pdf")
-        self.logger.info('Saving PDF %s', path)
-        with open(path, 'wb') as file:
-            file.write(response.body)
+
+
+
+
     # path = os.path.join(os.getcwd(),"docs","123.pdf")
+
+
+
+
+
