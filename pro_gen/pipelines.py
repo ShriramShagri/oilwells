5c49c2624b6183199c509e272a1f0fab5b33f83c
ShriramShagri <shagrisrm@gmail.com>
2020-10-02 14:01:43 +0530

    Fixed wh

--- pro_gen/pipelines.py
+++ pro_gen/pipelines.py
@@ -22,24 +22,56 @@ class ProGenPipeline:
 
 
     def process_item(self, item, spider):
-        # if wh table is present, pass proper param
+
+    # if wh table is present, pass proper param
 
         whRawData = [i.replace('\n' ,"") for i in item['wh']]
+        whFilteredData = []
+        extra = False
 
-        if whRawData[18] == '\n':
-            whFilteredData = whRawData[1:14:2] + whRawData[15:17] + whRawData[18:25:2] +whRawData[27::2]
-            self.store_wh(whFilteredData, False)
+        whFilteredData.append(whRawData[whRawData.index('API: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('KID: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Lease:') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Well:') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Original operator:') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Current operator:') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Field:') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Location: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Location: ') + 2])
+        if whRawData[whRawData.index('Location: ') + 2] != '':
+            whFilteredData.append(whRawData[whRawData.index('Location: ') + 3])
+            extra = True
+        whFilteredData.append(whRawData[whRawData.index('NAD27 Longitude: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('NAD27 Latitude: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('NAD83 Longitude: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('NAD83 Latitude: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('County: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Permit Date: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Spud Date: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Completion Date: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Plugging Date: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Well Type: ') + 1])
+        try:
+            whFilteredData.append(whRawData[whRawData.index('Status: ') + 1])
+        except:
+            whFilteredData.append("")
+        whFilteredData.append(whRawData[whRawData.index('Total Depth: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Elevation: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('Producing Formation: ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('IP Oil (bbl): ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('IP Water (bbl): ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('IP GAS (MCF): ') + 1])
+        whFilteredData.append(whRawData[whRawData.index('KCC Permit No.: ') + 1])
+
+        self.store_wh(whFilteredData, extra)
             
-        else:
-            whFilteredData = whRawData[1:14:2] + whRawData[15:18] + whRawData[19:26:2] +whRawData[28::2]
-            self.store_wh(whFilteredData, True)
 
         essentials = [whFilteredData[0], whFilteredData[1]]
 
 
         # if casing table is present, pass proper param
 
-        if item['casing']:
+        try:
             if 'Purpose of String' in item['casing']:
                 casingRawData = item['casing'][item['casing'].index('Additives')+1:]
                 casingFilteredData = []
@@ -51,50 +83,39 @@ class ProGenPipeline:
                     for i in range(len(casingRawData)//8):
                         casingFilteredData.append(essentials + casingRawData[i*8:(i+1)*8])
                     self.store_casing(casingFilteredData)
+        except:
+            pass
+        
+
+        try:
+            if 'Perforation record' in item['pf']:
+                pfRawData = item['pf'][item['pf'].index('Depth')+1:]
+                pfFilteredData = []
+                try:
+                    checklist = [int(pfRawData[i*4])for i in range(len(pfRawData)//4)]
+                    repeat = 4
+                    print("Zone 4")
+                    addition = []
+                except:
+                    repeat = 3
+                    print("Zone 3")
+                    addition = [""]
+
+                for i in range(len(pfRawData)//repeat):
+                    pfFilteredData.append(essentials + pfRawData[i*repeat:(i+1)*repeat] + addition)
+                self.store_pf(pfFilteredData)
+
+        except:
+            pass
 
         return item
 
+
     def store_wh(self, item, extra):
         if extra:
-            while len(item)>28:
-                del item[-2]
-            sql = '''
-            INSERT INTO WH 
-            (API,
-            KID,
-            Lease,
-            Well,
-            Original_operator,
-            Current_operator,
-            Field,
-            Location1,
-            Location2,
-            Location3,
-            NAD27_Longitude,
-            NAD27_Latitude,
-            NAD83_Longitude,
-            NAD83_Latitude,
-            County,
-            Permit_Date,
-            Spud_Date,
-            Completion_Date,
-            Plugging_Date,
-            Well_Type,
-            Status,
-            Total_Depth,
-            Elevation,
-            Producing_Formation,
-            IP_Oil,
-            IP_Water,
-            IP_GAS,
-            KCC_Permit_No)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            '''
-            self.cur.execute(sql, tuple(item))
+            self.cur.execute("INSERT INTO WH VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" , tuple(item))
             self.conn.commit()
         else:
-            while len(item)>=28:
-                del item[-2]
             sql = '''
             INSERT INTO WH 
             (API,
@@ -165,34 +186,11 @@ class ProGenPipeline:
         self.conn.commit()
     
     def store_casing(self, item):
-        # sql = '''
-        # INSERT INTO IP
-        # (API
-        # KID,
-        # Purpose_Of_String,
-        # Size_Hole_Drilled
-        # Size_Casing_Set
-        # Weight
-        # Setting_Depth,
-        # Type_Of_Cement,
-        # Sacks_Used
-        # Type_And_Percent_Additives)
-        # VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        # '''
         args_str = b','.join(self.cur.mogrify("(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple(x)) for x in item).decode("utf-8") 
         self.cur.execute("INSERT INTO casing VALUES " + args_str) 
-        # self.cur.execute(sql, tuple(item))
         self.conn.commit()
     
-    def store_perforation(self, item, kid):
-        sql = '''
-        INSERT INTO IP
-        KID,
-        Shots_Per_Foot,
-        Perforation_Record,
-        Material_Record,
-        Depth 
-        VALUES (%s, %s, %s, %s, %s)
-        '''
-        self.cur.execute(sql, tuple(kid) + tuple(item))
+    def store_pf(self, item):
+        args_str = b','.join(self.cur.mogrify("(%s, %s, %s, %s, %s, %s)", tuple(x)) for x in item).decode("utf-8") 
+        self.cur.execute("INSERT INTO Perforation VALUES " + args_str) 
         self.conn.commit()
