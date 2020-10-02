9cdcbf236c1fb42ac00a7705609b9922cf0c3fe0
ShriramShagri <shagrisrm@gmail.com>
2020-10-02 14:04:07 +0530

    Updated wh

--- pro_gen/pipelines.py
+++ pro_gen/pipelines.py
@@ -24,7 +24,6 @@ class ProGenPipeline:
 
     # if wh table is present, pass proper param
 
-<<<<<<< HEAD
         whRawData = [i.replace('\n' ,"") for i in item['wh']]
         whFilteredData = []
         extra = False
@@ -65,17 +64,6 @@ class ProGenPipeline:
 
         self.store_wh(whFilteredData, extra)
             
-=======
-        whRawData = [i.replace('\n', "") for i in item['wh']]
-
-        if whRawData[18] == '\n':
-            whFilteredData = whRawData[1:14:2] + whRawData[15:17] + whRawData[18:25:2] + whRawData[27::2]
-            self.store_wh(whFilteredData, False)
-
-        else:
-            whFilteredData = whRawData[1:14:2] + whRawData[15:18] + whRawData[19:26:2] + whRawData[28::2]
-            self.store_wh(whFilteredData, True)
->>>>>>> d7d5eb25cf67a065cd23f0c475108e2f52b0e787
 
         essentials = [whFilteredData[0], whFilteredData[1]]
 
@@ -95,7 +83,6 @@ class ProGenPipeline:
                     self.store_casing(casingFilteredData)
         except:
             pass
-<<<<<<< HEAD
         
 
         try:
@@ -119,58 +106,14 @@ class ProGenPipeline:
         except:
             pass
 
-=======
->>>>>>> d7d5eb25cf67a065cd23f0c475108e2f52b0e787
         return item
 
 
     def store_wh(self, item, extra):
         if extra:
-<<<<<<< HEAD
             self.cur.execute("INSERT INTO WH VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" , tuple(item))
             self.conn.commit()
         else:
-=======
-            while len(item) > 28:
-                del item[-2]
-            sql = '''
-            INSERT INTO WH 
-            (API,
-            KID,
-            Lease,
-            Well,
-            Original_operator,
-            Current_operator,
-            Field,
-            Location1,
-            Location2,
-            Location3,
-            NAD27_Longitude,
-            NAD27_Latitude,
-            NAD83_Longitude,
-            NAD83_Latitude,
-            County,
-            Permit_Date,
-            Spud_Date,
-            Completion_Date,
-            Plugging_Date,
-            Well_Type,
-            Status,
-            Total_Depth,
-            Elevation,
-            Producing_Formation,
-            IP_Oil,
-            IP_Water,
-            IP_GAS,
-            KCC_Permit_No)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            '''
-            self.cur.execute(sql, tuple(item))
-            self.conn.commit()
-        else:
-            while len(item) >= 28:
-                del item[-2]
->>>>>>> d7d5eb25cf67a065cd23f0c475108e2f52b0e787
             sql = '''
             INSERT INTO WH 
             (API,
@@ -240,7 +183,6 @@ class ProGenPipeline:
         self.conn.commit()
 
     def store_casing(self, item):
-<<<<<<< HEAD
         args_str = b','.join(self.cur.mogrify("(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple(x)) for x in item).decode("utf-8") 
         self.cur.execute("INSERT INTO casing VALUES " + args_str) 
         self.conn.commit()
@@ -248,37 +190,4 @@ class ProGenPipeline:
     def store_pf(self, item):
         args_str = b','.join(self.cur.mogrify("(%s, %s, %s, %s, %s, %s)", tuple(x)) for x in item).decode("utf-8") 
         self.cur.execute("INSERT INTO Perforation VALUES " + args_str) 
-=======
-        # sql = '''
-        # INSERT INTO IP
-        # (API
-        # KID,
-        # Purpose_Of_String,
-        # Size_Hole_Drilled
-        # Size_Casing_Set
-        # Weight
-        # Setting_Depth,
-        # Type_Of_Cement,
-        # Sacks_Used
-        # Type_And_Percent_Additives)
-        # VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        # '''
-        args_str = b','.join(
-            self.cur.mogrify("(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple(x)) for x in item).decode("utf-8")
-        self.cur.execute("INSERT INTO casing VALUES " + args_str)
-        # self.cur.execute(sql, tuple(item))
-        self.conn.commit()
-
-    def store_perforation(self, item, kid):
-        sql = '''
-        INSERT INTO IP
-        KID,
-        Shots_Per_Foot,
-        Perforation_Record,
-        Material_Record,
-        Depth 
-        VALUES (%s, %s, %s, %s, %s)
-        '''
-        self.cur.execute(sql, tuple(kid) + tuple(item))
->>>>>>> d7d5eb25cf67a065cd23f0c475108e2f52b0e787
         self.conn.commit()
