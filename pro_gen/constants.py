235a136b71426dac99463002a8a2f034e2cbde25
ShriramShagri <shagrisrm@gmail.com>
2020-11-07 10:34:22 +0530

    error updates

--- pro_gen/constants.py
+++ pro_gen/constants.py
@@ -1,20 +1,20 @@
-import psycopg2
+# import psycopg2
 import os
 
 # Database Class
 # Change the table values as you like
-class DB:
-    '''
-    Connect to database here in constants.py so that db can be accessed inside spider too
-    '''
-    def __init__(self):
-        self.conn = psycopg2.connect(
-            host='localhost',
-            database= 'oilwells',
-            user= 'postgres',
-            password= '123456'
-        )
-        self.cur = self.conn.cursor()
+# class DB:
+#     '''
+#     Connect to database here in constants.py so that db can be accessed inside spider too
+#     '''
+#     def __init__(self):
+#         self.conn = psycopg2.connect(
+#             host='localhost',
+#             database= 'oilwells',
+#             user= 'postgres',
+#             password= '123456'
+#         )
+#         self.cur = self.conn.cursor()
 
 # db object
 
